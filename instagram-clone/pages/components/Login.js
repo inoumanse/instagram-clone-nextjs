b7ac92190b76ad4ac80218875a5c931decab73b7
Nouman <inouman.se@gmail.com>
2022-12-03 23:40:11 +0500

    LoginPage completed

--- instagram-clone/pages/components/Login.js
+++ instagram-clone/pages/components/Login.js
@@ -28,11 +28,11 @@ const Login = () =>{
                 <form ref={formRef} className="w-64 flex flex-col gap-1 mt-8">
                     <div className="relative">
                         <input onChange={handleChange} name="username" type="text" className="peer w-full rounded border bg-gray-100 p-2 text-xs placeholder-transparent"
-                                placeholder="Phone number,username or email"></input>
+                                placeholder="Phone number, username or email"></input>
                         <label className="absolute transition-all left-2 top-0 text-gray-400 text-xxs 
                                             peer-placeholder-shown:text-xs
                                             peer-placeholder-shown:top-2
-                                            pointer-events-none">Phone number,username or email</label>
+                                            pointer-events-none">Phone number, username or email</label>
                     </div>
                     <div className="relative">
                         <input onChange={handleChange} name="password" type={showPassword ? "text" : "password"}
@@ -47,9 +47,30 @@ const Login = () =>{
                     </div>
                     <a className={`mt-2 text-sm text-center ${hasFilledBoth ? "bg-blue-500" : "bg-blue-300"} text-white py-1 rounded font-medium`}>Log in</a>
                 </form>
+                <div className="flex space-x-2 w-64 mt-4 items-center">
+                    <span className="bg-gray-300 h-px flex-1"></span>
+                    <span className="p-2 uppercase text-xs text-gray-400 font-semibold">or</span>
+                    <span className="bg-gray-300 h-px flex-1"></span>
+                </div>
+                <button className="mt-4 flex">
+                    <div className="bg-no-repeat facebook-logo mr-1"></div>
+                    <span className="text-xs text-blue-900 font-semibold">Log in with Facebook</span>
+                </button>
+                <a className="text-xs text-blue-900 mt-4 cursor-pointer">Forgot Password</a>
             </div>
-            <div className="bg-white">
-                Another Div
+            
+
+            <div className="bg-white w-80 border border-gray-300 text-center py-4">
+                <span className="text-sm mr-2">Don't have an Account?</span>
+                <a href="" className="text-blue-500 text-sm font-semibold">Sign up</a>
+            </div>
+            <div className="mt-3 text-center">
+                <span className="text-xs">Get the App</span>
+                <div className="mt-3 flex space-x-2">
+                    <div className="bg-no-repeat apple-store-logo"></div>
+                    <div className="bg-no-repeat google-store-logo"></div>
+                </div>
+
             </div>
         </div>
     )
