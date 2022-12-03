4805ea2037651b235302f7818bfbe5621484a2b4
Nouman <inouman.se@gmail.com>
2022-12-04 01:38:53 +0500

    making header

--- instagram-clone/pages/components/Header.js
+++ instagram-clone/pages/components/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
+import {SearchIcon} from "@heroicons/react/outline"
+import {HomeIcon, PlusCircleIcon} from "@heroicons/react/solid";
 
 const Header = () => {
     return(
         <div className="shadow-sm border-b bg-white top-0 fixed w-full">
-            <Image src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-                    layout="fill" objectFit="contain"/>
+            <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto p-2">
+                <div className="relative w-24 h-8 lg-inline-grid">
+                    <Image src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                        layout="fill" objectFit="contain"/>
+                </div>
+                <div className="relative mt-1 p-1 pl-1 rounded-sm sm:text-sm
+                                border-solid border-[1.2px] border-gray-300 flex bg-gray-50">
+                    <SearchIcon className="h-5 w-5 text-gray-400"></SearchIcon>
+                    <input type="text" placeholder="search" className="headerinput pl-1
+                            bg-gray-50 focus:outline-none"></input>
+                    <PlusCircleIcon className="h-5 w-5 text-gray-400 rotate-45"></PlusCircleIcon>
+
+                </div>
+            </div>
         </div>
     )
 }
--- instagram-clone/pages/index.js
+++ instagram-clone/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
+//import Image from 'next/image'
+import Image from 'next/legacy/image'
 import styles from '../styles/Home.module.css'
 import Login from './components/Login'
 import Header from './components/Header'
